64af904cdca4dab4ea345ef4c7669424bb33e0ff
ayomide-shinaaluko <shinaalukoayomide@yahoo.co.uk>
2023-09-02 22:57:46 +0100

    import tasks and requirements

---
+++ ml-model/README.md
@@ -0,0 +1,23 @@
+# Car price prediction
+
+The python file trains a machine learning model to predict the car price in Spain based on the car type, age, fuel type, mileage and gearbox type. When you run this python notebook, it will create and save the trained model in the directory. You can use this model in your final product.
+
+## Task1
+
+Build a web interface/web application/API that predicts car prices based on user inputs include make, model year, fuel type, gear type, and mileage using the provided pre-trained machine learning model.
+
+## Requirements
+
+1. Load the pre-trained car price prediction model created by this python notebook.
+2. Create a web interface/web application/API that accepts the following user inputs:
+   1. Car make (e.g. BMW)
+   2. Model year (e.g. 2018)
+   3. Fuel type (e.g. Diesel)
+   4. Transmission type (e.g. Manual)
+   5. Mileage (e.g. 10000)
+3. Pass the user inputs to the pre-trained model to generate a predicted car price
+4. Display the predicted price back to the user
+
+## Task2
+
+What techniques could be used to further improve this ML model performance (e.g. better prediction accuracy, better generalisation ability against overfitting, etc.)? If so, can you apply the techniques you'd propose to re-train another ML model and compare the difference?
---
+++ ml-model/car_price_prediction.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import datetime
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from joblib import dump
+
+# Load the dataset
+df = pd.read_csv("used_cars_data.csv")
+
+# One-hot encode categorical features
+enc = OneHotEncoder(handle_unknown='ignore')
+X = df[['brand', 'fuel', 'gearbox']]
+enc.fit(X)
+X_features = pd.DataFrame(enc.transform(X).toarray())
+
+# Feature engineering
+year = datetime.datetime.now().year
+df['age'] = year - df['year']
+X_num = df[['age', 'mileage (kms)']]
+y = df['price (eur)']
+
+# Combine numerical and one-hot encoded features
+X = pd.concat([X_num, X_features], axis=1)
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+# Initialize and train the random forest regressor model
+regr = RandomForestRegressor()
+regr.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = regr.predict(X_test)
+
+# Model evaluation - Mean Squared Error
+mse = mean_squared_error(y_test, y_pred, squared=False)
+print(f"Root Mean Squared Error: {mse:.2f}")
+
+# Model evaluation - Mean Absolute Error
+mae = mean_absolute_error(y_test, y_pred)
+print(f"Mean Absolute Error: {mae:.2f}")
+
+# Save the trained model to a file
+dump(regr, 'car_price_prediction_model.joblib')
